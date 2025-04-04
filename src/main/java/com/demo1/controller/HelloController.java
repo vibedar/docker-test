42fb1f11a1c13bee8c76109e39f564e0142c1db3
nobody <mr.nobodyvibe@gmail.com>
2025-04-04 02:43:35 -0700

    demo-app-v1

---
+++ src/main/java/com/demo1/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.demo1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/message")
+	public String getMesaage() {
+		return "hello-world";
+	}
+
+}
---
+++ src/test/java/com/demo1/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
